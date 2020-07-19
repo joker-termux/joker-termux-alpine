6219d4c19b8005a77ed1cceea9b9076b87aeec92
joker-termux <68482235+joker-termux@users.noreply.github.com>
2020-07-19 17:44:47 +0200

    Update joker-alpine.sh

--- joker-alpine.sh
+++ joker-alpine.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash -e
 # Copyright ©2019 by Hax4Us. All rights reserved.
 #
-# Email : mojoker9999@gmail.com
+# Email : mojoker9999@gmail.co
 ################################################################################
 
 # colors
